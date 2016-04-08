c1406a24ba99bf2aff2b6e8ed8caf3172b1ff024
chris berry <chrisberry321@yahoo.com>
2016-04-08 09:39:17 -0700

    set up about, index, firebase, question-tile, and the ability to hide and show notes.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-code-review',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://inpdx-ember-code-rev.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
