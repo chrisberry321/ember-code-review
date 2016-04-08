57a3cdfb8b54eca89b5676f64d2f9d933f2b2fb3
chris berry <chrisberry321@yahoo.com>
2016-04-08 15:36:45 -0700

    routes/index.js is now working

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+    questions: this.store.findAll('question'),
+    responses: this.store.findAll('response'),
+  });
   },
   actions: {
     save3(params) {
