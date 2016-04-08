75dc98665f7f5ff8fa80104b30e9dfa1779c32a0
chris berry <chrisberry321@yahoo.com>
2016-04-08 12:34:42 -0700

    styles on index questions are functional

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,14 +6,10 @@ export default Ember.Route.extend({
     return this.store.findAll('question');
   },
   actions: {
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    },
     save3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    }
+    },
   }
 });
