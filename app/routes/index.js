2f75388a7dee7c46528faefef202e1b48a7cea0b
chris berry <chrisberry321@yahoo.com>
2016-04-08 10:08:10 -0700

    form for generating new question, not linking to index at the moment

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,15 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+    save3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
