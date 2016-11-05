577137b142dfbb294419a69b34b791e8c8efc877
chris <chrisberry321@yahoo.com>
2016-11-05 14:15:12 -0700

    Added ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -53,6 +53,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.baseUrl = '/ember-code-review/';
+    ENV.locationType = 'hash';
+    ENV.baseUrl = '/ember-code-review/';
 
   }
 
