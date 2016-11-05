927e975b306600439c9b161ab67be56183a157c8
chris <chrisberry321@yahoo.com>
2016-11-05 14:46:41 -0700

    initial gh-pages commit

--- app/models/question.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  submit: DS.attr(),
-  author: DS.attr(),
-  notes: DS.attr(),
-  responses: DS.hasMany('response', { async: true})
-});
--- app/models/response.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  author: DS.attr(),
-  answer: DS.attr(),
-  question: DS.belongsTo('question', { async: true}),
-});
